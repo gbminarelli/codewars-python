a33f83e694f403335b4b483ecd925a06c70c7a3c
Gian <34799380+gbminarelli@users.noreply.github.com>
2026-03-05 15:18:12 -0300

    Refactor who_likes_it.py

--- 6Kyu/who_likes_it.py
+++ 6Kyu/who_likes_it.py
@@ -5,11 +5,10 @@ def likes(names):
     likes_count = len(names)
     if likes_count > 3:
         return f"{names[0]}, {names[1]} and {likes_count - 2} others like this"
-    elif likes_count == 3:
+    if likes_count == 3:
         return f"{names[0]}, {names[1]} and {names[2]} like this"
-    elif likes_count == 2:
+    if likes_count == 2:
         return f"{names[0]} and {names[1]} like this"
-    elif likes_count == 1:
+    if likes_count == 1:
         return f"{names[0]} likes this"
-    else:
-        return "no one likes this"
+    return "no one likes this"
