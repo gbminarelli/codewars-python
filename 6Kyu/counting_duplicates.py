6ee673e6de2106c5808d409dadee3ec1fcec8a9d
Gian <34799380+gbminarelli@users.noreply.github.com>
2026-03-19 16:13:21 -0300

    Add counting_duplicates.py

---
+++ 6Kyu/counting_duplicates.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
+
+
+import collections
+
+
+def duplicate_count(text):
+    return sum(1 for _, count in collections.Counter(text.lower()).items() if count > 1)
