351a559750b3c62296d2284e9f0df77dc4727772
Gian <34799380+gbminarelli@users.noreply.github.com>
2026-03-22 23:55:13 -0300

    Refactor sudoku_solver.py to cleanup code and fix formatting

--- 3Kyu/sudoku_solver.py
+++ 3Kyu/sudoku_solver.py
@@ -1,11 +1,11 @@
 # https://www.codewars.com/kata/5296bc77afba8baa690002d7
 
+
 def sudoku(puzzle):
     puzzle_solved = [e[:] for e in puzzle]
     r = [set(range(1, 10)) for _ in range(9)]
     c = [set(range(1, 10)) for _ in range(9)]
     s = [[set(range(1, 10)) for _ in range(3)] for _ in range(3)]
-    # zeros = sum(1 for row in puzzle for e in row if not e)
     zeros = 0
     for i, row in enumerate(puzzle):
         for j, e in enumerate(row):
@@ -28,20 +28,3 @@ def sudoku(puzzle):
                         zeros -= 1
                         puzzle_solved[i][j] = new_e
     return puzzle_solved
-
-
-puzzle = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-]
-
-print(sudoku(puzzle))
-
-# print(sum(1 for e in [[1, 2, 0, 3, 0, 3], [1, 2, 0]] for a in e if not a))
