af4b5f7a11879cb4a33df4eb6f899e93c41e9f41
Gian <34799380+gbminarelli@users.noreply.github.com>
2026-04-18 14:56:00 -0300

    Add primes_in_numbers.py

---
+++ 5Kyu/primes_in_numbers.py
@@ -0,0 +1,22 @@
+# https://www.codewars.com/kata/54d512e62a5e54c96200019e
+
+import collections
+
+
+def format_prime_factors(f):
+    return "".join(f"({k}{f'**{v}' if v > 1 else ''})" for k, v in f.items())
+
+
+def prime_factors(n):
+    m = n
+    i = 2
+    f = collections.defaultdict(int)
+    while m >= i * i:
+        if m % i:
+            i += 1
+        else:
+            m //= i
+            f[i] += 1
+    if m > 1:
+        f[m] += 1
+    return format_prime_factors(f)
