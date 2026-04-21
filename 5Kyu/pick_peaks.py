abfb6f9918fb775bad8900bc691eeb818efde969
Gian <34799380+gbminarelli@users.noreply.github.com>
2026-04-21 11:27:01 -0300

    Add pick_peaks.py

---
+++ 5Kyu/pick_peaks.py
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/5279f6fe5ab7f447890006a7
+
+
+def pick_peaks(arr):
+    p = {"pos": [], "peaks": []}
+    if not arr:
+        return p
+    a = arr[0]
+    p_index = None
+    for i in range(1, len(arr)):
+        b = arr[i]
+        if a < b:
+            p_index = i
+        elif a > b and p_index is not None:
+            p["pos"].append(p_index)
+            p["peaks"].append(a)
+            p_index = None
+        a = b
+    return p
