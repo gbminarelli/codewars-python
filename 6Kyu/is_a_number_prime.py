b81c24640fcf2f54c6ee616c318103d6f309ed41
Gian <34799380+gbminarelli@users.noreply.github.com>
2026-04-22 23:04:49 -0300

    Add is_a_number_prime.py

---
+++ 6Kyu/is_a_number_prime.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/5262119038c0985a5b00029f
+
+import math
+
+
+def is_prime(num):
+    if num == 2:
+        return True
+    if num < 2 or num % 2 == 0:
+        return False
+    for i in range(3, math.isqrt(num) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
