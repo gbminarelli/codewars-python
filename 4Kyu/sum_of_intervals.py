42833d82cfe2994fef17eecee65ee8a39b1fbeac
Gian <34799380+gbminarelli@users.noreply.github.com>
2026-04-26 14:56:05 -0300

    Simplify interval overlap check by removing redundant continue statement

--- 4Kyu/sum_of_intervals.py
+++ 4Kyu/sum_of_intervals.py
@@ -5,8 +5,7 @@ def sum_of_intervals(intervals):
     s = 0
     top = float("-inf")
     for a, b in sorted(intervals):
-        if top > b:
-            continue
-        s += b - max(a, top)
-        top = b
+        if top < b:
+            s += b - max(a, top)
+            top = b
     return s
